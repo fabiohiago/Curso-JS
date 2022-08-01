1646e064aa2239be500b60600b62ed1f7552af7e
fabi-hiago <fabiohr1600@gmail.com>
2022-08-01 01:12:33 -0300

    .

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**************AULA 1**************/
+//Operador de exponenciação
+const raio = 5.6
+const area = Map.PI * Math.pow(raio, 2)
+
+//console.log(area)
+//console.log(typeof Math)
+/*********************************/
+
+/**************AULA 1**************/
+
+/*********************************/
+
+/**************AULA 3**************/
+
+//O JS retornara o valor verdadeiro, no caso a string preechida
+let nome = 'Fabio'
+//console.log( nome || 'Desconhecido')
+
+/*********************************/
